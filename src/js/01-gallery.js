144ceb8cd567b5ee2c9d9648412b110cefc21574
Illia23 <Priymak2331@gmail.com>
2023-09-18 20:27:43 -0500

    commit home work 08 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const list = document.querySelector(".gallery");
+const createItems = createMarkUp(galleryItems);
+list.insertAdjacentHTML("beforeend", createItems);
 
+function createMarkUp(items) {
+    return items.map(({preview, original, description}) => {
+      return  `<a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}" 
+        alt="${description}">
+       </a>` 
+    }).join("");
+}
+
+const simple = new SimpleLightbox(".gallery a", {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+
+});
 console.log(galleryItems);
